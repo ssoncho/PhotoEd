c51c6fce41c9774be36b13ecdeaed74f21c80461
Sofia <sofia.sofia>
2023-12-21 12:47:04 +0500

    Add icons to buttons

--- drawing_area.py
+++ drawing_area.py
@@ -2,70 +2,6 @@ from PyQt6.QtWidgets import (QLabel, QFileDialog, QGraphicsView, QGraphicsScene,
 from PyQt6.QtGui import (QPixmap, QPainter, QPen, QColor, QImage)
 from PyQt6.QtCore import (Qt, QPoint, QLineF, QPointF, QRectF)
 
-# class DrawingArea(QLabel):
-#     def __init__(self):
-#         super().__init__()
-#         self.pixmap = QPixmap(self.size())
-#         self.pixmap.fill(Qt.GlobalColor.white)
-#         self.setPixmap(self.pixmap)
-#         self.last_x, self.last_y = None, None
-#         self.setMinimumSize(50,50)
-#         self.pen_color = Qt.GlobalColor.blue
-
-#     def add_image(self):
-#         image = QPixmap("dog.jpg")
-#         self.pixmap = image.scaled(self.size(), Qt.AspectRatioMode.KeepAspectRatio)
-#         self.setPixmap(self.pixmap)
-#         self.resize(self.pixmap.size())
-#         # file_name, _ = QFileDialog.getOpenFileName(self, "Add Image", "")
-#         # if file_name:
-#         #     image = QPixmap(file_name)
-#         #     self.pixmap = image.scaled(self.size(),
-#         #      Qt.AspectRatioMode.KeepAspectRatio, 
-#         #      Qt.TransformationMode.SmoothTransformation)
-#         #     self.setPixmap(self.pixmap)
-
-#     def save_image(self):
-#         file_name, _ = QFileDialog.getSaveFileName(self, "Save Image", "", "Images (*.png *.jpg *.bmp)")
-#         if file_name:
-#             self.pixmap.save(file_name)
-
-#     def set_pen_color(self, color: QColor):
-#         self.pen_color = color
-
-#     def mouseMoveEvent(self, e):
-#         if self.last_x is None: # First event.
-#             self.last_x = e.position().x()
-#             self.last_y = e.position().y()
-#             return # Ignore the first time.
-
-#         painter = QPainter(self.pixmap)
-#         pen = QPen(self.pen_color, 7)
-#         pen.setCapStyle(Qt.PenCapStyle.RoundCap)
-#         painter.setPen(pen)
-#         line = QLineF(self.last_x, self.last_y, e.position().x(), e.position().y())
-#         painter.drawLine(line)
-#         painter.end()
-#         self.setPixmap(self.pixmap)
-
-#         # Update the origin for next time.
-#         self.last_x = e.position().x()
-#         self.last_y = e.position().y()
-
-#     def mouseReleaseEvent(self, e):
-#         self.last_x = None
-#         self.last_y = None
-
-#     def resizeEvent(self, e):
-#         newPixmap = self.pixmap.scaled(e.size())
-#         newPixmap.fill(Qt.GlobalColor.white)
-#         painter = QPainter(newPixmap)
-#         painter.drawPixmap(QPoint(), self.pixmap)
-#         self.pixmap = newPixmap
-
-#         self.setPixmap(self.pixmap)
-#         super().resizeEvent(e)
-
 class HandDrawingScene(QGraphicsScene):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -103,9 +39,9 @@ class Viewer(QGraphicsView):
         self.m_hand_drawing_scene = HandDrawingScene(self)
         self.setScene(self.m_hand_drawing_scene)
         self.m_pixmapItem = self.scene().addPixmap(QPixmap(self.size()))
-        white_pixmap = self.pixmap
-        white_pixmap.fill(Qt.GlobalColor.lightGray)
-        self.pixmap = white_pixmap
+        initial_pixmap = self.pixmap
+        initial_pixmap.fill(Qt.GlobalColor.lightGray)
+        self.pixmap = initial_pixmap
 
         self.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
